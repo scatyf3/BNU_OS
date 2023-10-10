ceaff83445f3976701d51a316b2311dfc3635d66
scat <suzuki1968@protonmail.com>
2023-10-10 11:36:21 +0800

    消息队列初稿

--- 消息队列实验/消息队列实验/例子/msg_queue.c
+++ 消息队列实验/消息队列实验/例子/msg_queue.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 #include <sys/types.h>
+#include <sys/msg.h> 
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
 
 #define MSGKEY 75
 #define MSGSIZE 256
 
+int CLIENT();
+int SERVER();
 
 int main(){
     /*
@@ -50,38 +57,69 @@ int main(){
 struct s_msg{
     long id;
     char msg_str[MSGSIZE];
+};
 
-}
-
-SERVER(){
-	int msgqid,pid,*pint,c1_pid,c2_pid;
+int SERVER(){
+	int msgqid,pid,c1_pid,c2_pid;
     struct s_msg msg1;
     struct s_msg msg2;
     if ((msgqid=msgget(MSGKEY,0777))==-1)     /*建立消息队列失败*/
 	{
-		printf("This message queue does not exist. \n",*pint);
-		return;
+		printf("This message queue does not exist. \n");
+		return 1;
 	}
     int flag1,flag2=1;
+    char buffer[100];
+    pid=getpid();
     while(flag1||flag2){
-        msgrcv(msgqid,&msg1,256,1,0);
-        msgrcv(msgqid,&msg2,256,1,0);
+        c1_pid=(msgqid,&msg1,256,1,0);
+        c2_pid=(msgqid,&msg2,256,1,0);
         //todo:get server and client pid somehow
+
         if(msg1.id==1){
-            printf("(SERVER %d) received message %d from CLIENT %d\n",pid,msg1.id,c1_pid);
+            snprintf(buffer, sizeof(buffer),"(SERVER %d) received message %d from CLIENT %d\n",pid,msg1.id,c1_pid);
             flag1=0;
             //todo:msgsnd
-            msgsnd(msgqid,&msg1,sizeof(msg1),0)
+            strcpy(msg1.msg_str, buffer);
+            msgsnd(msgqid,&msg1,sizeof(msg1),0);
         }
         if(msg2.id==1){
-            printf("(SERVER %d) received message %d from CLIENT %d\n",pid,msg2.id,c2_pid);
+            snprintf(buffer, sizeof(buffer),"(SERVER %d) received message %d from CLIENT %d\n",pid,msg2.id,c2_pid);
             flag2=0;
+            strcpy(msg2.msg_str, buffer);
+            msgsnd(msgqid,&msg2,sizeof(msg2),0);
         }
     }
 
 }
 
-CLIENT(){
+int CLIENT(){
+    /*
+    CLIENT1端使用key为75的消息队列，先后发送消息编号从10到1的消息，然后退出。
+    CLIENT1每发送一条消息后显示一句“(CLIENT1 CLIENT1端pid) sent message 消息的编号”,
+    然后等待接受SERVER端返回给自己的消息后，显示一句
+    “(CLIENT1 CLIENT1端pid) received return message 消息的编号 from SERVER端pid”，
+    再发送下一条消息。
+    */
+    int msgqid;
+    if ((msgqid=msgget(MSGKEY,0777))==-1)     /*建立消息队列失败*/
+	{
+		printf("This message queue does not exist. \n");
+		exit(1);
+	}
+
+   struct s_msg msg;
+   strcpy(msg.msg_str,"Some random msg");
+   for(int i=10;i>0;i--){
+        // 对消息内容进行赋值
+        sprintf(msg.msg_str, "(CLIENT1 %d) sent message %d\n", getpid(), i);
+        msgsnd(msgqid, &msg, 1024, 0); // 发送消息 msg 到 msgid 消息队列
+
+        printf("(CLIENT1 %d) sent message %d\n", getpid(), i);
+
+        msgrcv(msgqid, &msg, 1024, 0, 0); // 接收消息
+        printf("(CLIENT1 %d) received return message %d from SERVER%d\n", getpid(), i, msg.id);
+   }
 
 
 }
